fe425801224ed50a532d4b89c1bf2ef78d935f50
QuirrelForU <symmlux@gmail.com>
2024-03-19 02:35:42 +0700

    working search func

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,16 +1,21 @@
+from selenium.webdriver import Keys
+
 from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
 
 
 class MainPage(BasePage):
     # Locators
+
     LOCATOR_CATALOG_BUTTON = (By.CSS_SELECTOR, 'button.nav-element__burger')
+
     LOCATOR_SPORT_CATEGORY_LINK = (By.XPATH, "//a[contains(@class, 'menu-burger__main-list-link--784')]")
     LOCATOR_SHOES_CATEGORY_LINK = (By.XPATH, "//a[contains(@class, 'menu-burger__main-list-link--629')]")
     LOCATOR_HOUSE_CATEGORY_LINK = (By.XPATH, "//a[contains(@class, 'menu-burger__main-list-link--258')]")
     LOCATOR_FURNITURE_CATEGORY_LINK = (By.XPATH, "//a[contains(@class, 'menu-burger__main-list-link--62827')]")
     LOCATOR_BOOKS_CATEGORY_LINK = (By.XPATH, "//a[contains(@class, 'menu-burger__main-list-link--519')]")
 
+    LOCATOR_SEARCH_INPUT = (By.ID, "searchInput")
     def open_sport_category(self):
         catalog_button = self.element_is_visible(self.LOCATOR_CATALOG_BUTTON)
         catalog_button.click()
@@ -45,3 +50,9 @@ class MainPage(BasePage):
 
         books_category_link = self.element_is_visible(self.LOCATOR_BOOKS_CATEGORY_LINK)
         books_category_link.click()
+
+    def search_item_enter(self, search_input):
+        search_bar = self.element_is_visible(self.LOCATOR_SEARCH_INPUT)
+        search_bar.clear()  # Очистка поля ввода перед вводом нового запроса
+        search_bar.send_keys(search_input)
+        search_bar.send_keys(Keys.ENTER)
--- tests/form_test.py
+++ tests/form_test.py
@@ -38,3 +38,9 @@ class TestMainPage:
         main_page.open_books_category()
         current_url = driver.current_url
         assert "/catalog/knigi" in current_url
+
+    def test_search_item_enter(self,driver):
+        main_page = MainPage(driver, "https://www.wildberries.ru/")
+        main_page.open()
+        main_page.search_item_enter("Чай")
+        time.sleep(5)
